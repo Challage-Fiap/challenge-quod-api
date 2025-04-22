a0ff325b8535fce7a3e2410d057e8769723bbcd4
santos_levi <levidossanttos@gmail.com>
2025-04-22 20:26:23 -0300

    first commit

---
+++ src/main/java/br/com/fiap/challenge/quod/ChallengeQuodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge.quod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeQuodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeQuodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/challenge/quod/ChallengeQuodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge.quod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeQuodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
